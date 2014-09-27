7ab764fbffc372bf8fa658c24d2f29c3391541a8
charlesamacon <charles.a.macon@gmail.com>
2014-09-27 02:49:41 -0500

    Prep work
    Added basic structure to macon.c

---
+++ SuperBash/macon.c
@@ -0,0 +1,17 @@
+/* Jordan Sanders        */
+/* Garrett Wescott       */
+/* Julie Quiroz          */
+/* Charles Alan Macon    */
+/*                       */
+/* CSCE 3600 - Fall 2014 */
+/*                       */
+/* SuperBash             */
+
+int Macon (char 2dArr[][])
+{
+	/* Just setting things up for later */
+	
+
+
+	return 0;
+}
