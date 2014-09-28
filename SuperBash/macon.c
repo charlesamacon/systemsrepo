e40f376338c450a61f5f38d6529d784c2ce0c03e
charlesamacon <charles.a.macon@gmail.com>
2014-09-28 02:46:28 -0500

    Pseudocode for Macon()
    Setting up how the function should work in pseudocode.

--- SuperBash/macon.c
+++ SuperBash/macon.c
@@ -7,11 +7,59 @@
 /*                       */
 /* SuperBash             */
 
-int Macon (char 2dArr[][])
+/* Check out http://www.cs.uregina.ca/Links/class-info/330/ParsingLine/parsingline.html */
+/* For info about how this program should handle strings. Very Helpful.					*/
+
+int main(blah blah)
 {
-	/* Just setting things up for later */
+	// int repeat = 0;
+	// char repeatTimes[1];
+	/* for each string in file
+		{
+			do stuff
+			
+			if (strcmp(currentString, "repeat") = 0)
+			{
+				repeat = 1;			
+			}
+			
+			if (repeat == 1)
+			{
+				Macon(currentString);
+				
+			}
+		}
+
+*/
+}
+
+char * Macon (char 2dArr[][])
+{
+	/* Just setting things up for later 
 	
+	 PseudoCode!
+	
+	 If (strcmp(currentString, "repeat") = 0)
+	{
+	then we're in a loop (set repeat "bool" to true). output:
+	
+	return "repeatIndex1=0\nwhile [ $repeatIndex1 -lt $num ]\n""
+	}	
+	I don't need to check what $num is (that's handled earlier)
 
+	REMEMBER: WE ARE JUST TRANSLATING THIS.
+	
+	At this point we need to check for a '{' character, which we then replace with "do"
+	{
+		return "do\n"
+	}
 
-	return 0;
+	Copy all the other crap ("echo whatever")
+	
+	When we hit a '}' character, we replace it with
+	{
+		return "repeatIndex1=$[$repeatIndex1+1]\ndone";	
+	}
+	
+	return 0;*/
 }
