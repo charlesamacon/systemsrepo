1f769862c0911a2d851f8fb0c8c395a5651fef7a
IRPepper <jms0666@unt.edu>
2014-09-29 01:15:34 -0500

    Updated main with notes
    First commit lacked notes within the file, added for additional
    readability. I'm not very good at describing what's going on, sorry!

--- SuperBash/main.c
+++ SuperBash/main.c
@@ -9,12 +9,11 @@ int main(int argc, char** argv){
 	int i,k,j,n;
 	k=0;
 	i=0;
-	j=0;
 	n=0;
 	
-	input = fopen(argv[1],"r");
+	input = fopen(argv[1],"r");  //Opens the file listed on the command line call for reading
 	
-	while((c=fgetc(input)) != EOF){
+	while((c=fgetc(input)) != EOF){  //Copies file to 2d character array string
 		if(c!='\n'){
 			string[k][i]=c;
 			i++;
@@ -25,13 +24,13 @@ int main(int argc, char** argv){
 			k++;
 		}
 	}
-	string[k][0]= EOF;
+	string[k][0]= EOF; //Puts EOF at end of the 2d char array
 	
 	fclose(input);
 	
-	output = fopen("output","w");
+	output = fopen("output","w"); //Creates output file and opens it for writing
 	
-	while(n<k){
+	while(n<k){ //Temporary code to check file copying and output
 		fputs(string[n],output);
 		n++;
 	}
@@ -41,14 +40,14 @@ int main(int argc, char** argv){
 	return 0;
 }
 /*
-Sanders(char** string){
+Sanders(char** string){  //Recursively searches input 2d character array for assignment objectives and passes them to their respective functions. Recursion is to catch nested statements.
 	int i = 0;
 	int k = 0;
 	int n=0;
 	char arr[100][100];
 	
-	while(string[k][i] != EOF){
-		if(string[k][i] == 'i' && string[k][i+1] == 'f'){
+	while(string[k][i] != EOF){ 
+		if(string[k][i] == 'i' && string[k][i+1] == 'f'){  //Searches for if statements
 			arr = Sanders(arr);
 			while(string[k][i] != 'f' && string[k][i+1] != 'i'){
 				arr[n]=string[k];
@@ -60,7 +59,7 @@ Sanders(char** string){
 			return arr;
 		}
 		
-		if(string[k][i] == 'r' && string[k][i+1] == 'e' && string[k][i] == 'p' && string[k][i+1] == 'a' && string[k][i] == 'e' && string[k][i+1] == 't'){
+		if(string[k][i] == 'r' && string[k][i+1] == 'e' && string[k][i] == 'p' && string[k][i+1] == 'a' && string[k][i] == 'e' && string[k][i+1] == 't'){  //Searches for repeat statements
 			arr = Sanders(arr);
 			while(string[k][i] != '}'){
 				arr[n]=string[k];
