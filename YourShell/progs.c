18715bc97599ce3cc5e2aa6d22e3a64a5dacbff6
charlesamacon <charles.a.macon@gmail.com>
2014-11-17 22:02:16 -0600

    Porting things over
    Ported SuperBash to the shell, fake echo, and added the content needed
    for man to work...not actually working yet, though.

--- YourShell/progs.c
+++ YourShell/progs.c
@@ -44,30 +44,228 @@ int tokenizer(char *c, char *argv[])
 }
 
 // Echo
-void echo()
+void echo(char *argv[], int argc)
 {
 	// Use printf to print arguments. If a $ is encountered, what follows should be made a variable.
+	int i = 0;
+
+	for (i; i < argc, i++)
+	{
+		printf("%s", argv[i]);			// This line is crap and doesn't help us.
+	}
+
 	return;
 }
 
 // Man
-void man()
+void man(char *argv[])
 {
 	// Output text files based on the argument name
+	// I'm thinking this should be done as simply as possible
+
+	char man[50];
+
+	// copy command from argv[] to man[]
+
+	// if (man == "echo")
+	//{
+	//	printf("ECHO			User Commands\n\n"
+	//		"Name\n"
+	//		"echo - display a line of text\n\n"
+	//		"Synopsis\n"
+	//		"echo[SHORT - OPTION]...[STRING]...\n"
+	//		"echo LONG - OPTION\n\n"
+	//		"Description\n\n"
+	//		"Echo the STRING(s) to standard output.\n\n"
+	//		"- n		do not output the trailing newline\n"
+	//		"- e 		enable interpretation of backslash escapes\n"
+	//		"- E 		disable interpretation of backslash escapes(default)\n"
+	//		"--help		display this page and exit\n");
+	//}
+	//else if (man == "cpusage")
+	//{
+	//	printf("cpusage			User Commands\n\n"
+	//		"Name\n"
+	//		"cpusage - Display the average CPU usage of the past 24 - hour period.\n\n"
+	//		"Synopsis\n"
+	//		"cpusage[SHORT - OPTION]...\n\n"
+	//		"Description\n\n"
+	//		"Displays the current average CPU usage of the past 24 - hour period.\n\n"
+	//		"This is associated with a background process that uses the top command\n"
+	//		"to get a 'snapshot' of the CPU usage every hour and log it.cpusage\n"
+	//		"is then able to access this log and return an averaged value from it.\n\n"
+	//		"Options are currently not implemented or decided upon.\n"
+	//		"- cL	clear log\n"
+	//		"--help	display this help page and exit\n")
+	//}
+	//else if (man == "man")
+	//{
+	//	printf("man			User Commands\n\n"
+	//		"Name\n"
+	//		"man - displays the manual page for a given command\n\n"
+	//		"Synopsis\n"
+	//		"man[SHORT - OPTION]...\n\n"
+	//		"Description\n\n"
+	//		"man is the system's manual pager. Each page argument given to man is\n"
+	//		"normally the name of a program, utility or function.The manual page\n"
+	//		"associated with each of these arguments is then found and displayed.\n\n"
+	//		"For the time being, this version of man will display the entire manual\n"
+	//		"page for a given command. Whether or not this will persist in the final\n"
+	//		"project remains to be seen. Short, sweet, and to the point right now.\n\n"
+	//		"Currently implemented man pages :\n"
+	//		"echo\n"
+	//		"cpusage\n"
+	//		"man\n\n"
+	//		"Examples\n\n"
+	//		"man ls\n"
+	//		"Display the manual page for the program ls.\n")
+	//}
+
+
 	return;
 }
 
 // cpuUsage
-void cpuUsage()
+void cpusage()
 {
 	// Figure out a way to call Top.
 	return;
 }
 
 // SuperBash
-void superBash()
+void superBash(int argc, char * argv[])
 {
 	// This is a line for line copy of the superbash program. Except we need to fix the segmentation faults.
+	FILE *input;
+	FILE *output;
+	char c;
+	char string[100][100];
+	char arr[100][100];
+	char placeHolder[3] = " \n";
+	int i, k, j, n;
+	k = 0;
+	i = 0;
+	n = 0;
+
+	//----------------------------Open File Input-----------------------------------
+
+	input = fopen(argv[1], "r");  //Opens the file listed on the command line call for reading
+
+	while ((c = fgetc(input)) != EOF){  //Copies file to 2d character array string
+		if (c != '\n'){
+			string[k][i] = c;
+			i++;
+		}
+		else{
+			string[k][i] = '\n';
+			i = 0;
+			k++;
+			if ((string[k - 1][0] == 'f' && string[k - 1][1] == 'i') || string[k - 1][0] == '}'){
+				strcpy(string[k], placeHolder);
+				k++;
+			}
+			/*if(string[k-2][0]=='}'){
+			strcpy(string[k],placeHolder);
+			k++;
+			}
+			*/
+		}
+	}
+	string[k][0] = EOF; //Puts EOF at end of the 2d char array
+
+	fclose(input);
+
+	//-----------------------------Repeat Fix------------------------------------
+
+	i = 0;
+	k = 0;
+	n = 0;
+
+	//Really simple way of doing this
+	char repeat[100] = "repeatIndex1=0\nwhile [ $repeatIndex1 - lt $num ]\n";
+	char doString[100] = "do\n";
+	char doneString[100] = "repeatIndex1=$[$repeatIndex1+1]\ndone\n";
+
+	while (string[k][i] != EOF)
+	{
+		if (string[k][i] == 'r' && string[k][i + 1] == 'e' && string[k][i + 2] == 'p' && string[k][i + 3] == 'e' && string[k][i + 4] == 'a' && string[k][i + 5] == 't')
+		{
+			strcpy(string[k], repeat);
+			k++;
+		}
+		else if (string[k][i] == '{')
+		{
+			strcpy(string[k], doString);
+			k++;
+		}
+		else if (string[k][i] == '}')
+		{
+			strcpy(string[k], doneString);
+			k++;
+		}
+		else k++;
+	}
+	//-----------------------------If Fix-----------------------------------------
+
+	i = 0;
+	k = 0;
+	n = 0;
+
+	while (string[k][i] != EOF){
+		while (string[k][i] != '\n'){
+			if (string[k][i] == 't' && string[k][i + 1] == 'h' && string[k][i + 2] == 'e' && string[k][i + 3] == 'n'){
+				string[k][i] = '\n';
+				string[k][i + 1] = ' ';
+				string[k][i + 2] = ' ';
+				string[k][i + 3] = ' ';
+				string[k][i + 4] = ' ';
+				string[k][i + 5] = ' ';
+				string[k][i + 6] = 't';
+				string[k][i + 7] = 'h';
+				string[k][i + 8] = 'e';
+				string[k][i + 9] = 'n';
+				string[k][i + 10] = '\n';
+				break;
+			}
+			i++;
+		}
+		i = 0;
+		k++;
+	}
+	//-----------------------------Declaration Fix--------------------------------
+
+	i = 0;
+	k = 0;
+	n = 0;
+	int before, after;
+	while (string[k][i] != EOF){
+		while (string[k][i] != '\n'){
+			if (string[k][i] == '='){
+				if (string[k][i - 1] == ' ' && string[k][i + 1] == ' '){
+					for (j = i - 1; string[k][j] != '\n'; j++){
+						string[k][j] = string[k][j + 1];
+					}
+					for (j = i; string[k][j] != '\n'; j++){
+						string[k][j] = string[k][j + 1];
+					}
+				}
+			}
+			i++;
+		}
+		i = 0;
+		k++;
+	}
+	//-----------------------------Write to Output-------------------------------
+
+	output = fopen("output", "w"); //Creates output file and opens it for writing
+
+	while (n<k){ //Temporary code to check file copying and output
+		fputs(string[n], output);
+		n++;
+	}
+
+	fclose(output);
+
 	return;
 }
 
